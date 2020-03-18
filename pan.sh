86888fb30be504bcb184c384a26dc98a743ada4a
pan <catifish@163.com>
2020-03-18 17:05:11 +0800

    add help command

--- pan.sh
+++ pan.sh
@@ -6,6 +6,8 @@
 declare -A commands=()
 commands["1"]="ls"
 commands["2"]="dir"
+commands["h"]="help"
+commands["help"]="help"
 
 # 常量配置
 version="0.0.1"
@@ -14,19 +16,39 @@ version="0.0.1"
 declare -A colors=(["none"]="\033[0m" ["black"]="\033[0;30m" ["red"]="\033[31m" ["green"]="\033[0;32m" ["yellow"]="\033[0;33m" ["blue"]="\033[0;34m" ["purple"]="\033[0;35m" ["lightblue"]="\033[0;36m" ["white"]="\033[0;37m")
 showGreen()
 {
+	echo -e "${colors['green']}$1${colors['none']}\c"
+}
+showGreenN()
+{
 	echo -e "${colors['green']}$1${colors['none']}"
 }
 showYellow()
+{
+	echo -e "${colors['yellow']}$1${colors['none']}\c"
+}
+showYellowN()
 {
 	echo -e "${colors['yellow']}$1${colors['none']}"
 }
 showRed()
 {
+	echo -e "${colors['red']}$1${colors['none']}\c"
+}
+showRedN()
+{
 	echo -e "${colors['red']}$1${colors['none']}"
 }
 showVersion()
 {
-	showGreen "Pan's shell script, version[$version]"
+	showGreenN "Pan's shell script, version[$version]"
+}
+help()
+{
+	showGreenN "命令数量:${#commands[@]}"
+	for c in ${!commands[@]}; do
+		showGreen "\t$c\t"
+		showYellowN "${commands[$c]}"
+	done
 }
 
 # 参数校验
